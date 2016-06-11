6393ac8385a5c8175027f08bdec09cfdab3a3130
Jenning123 <962223482@qq.com>
2016-06-11 16:24:06 +0800

    Create main.cpp

---
+++ src/Interpreter/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include"Interpreter.h"
+#include"API.h"
+#include<stdio.h>
+
+int main()
+{
+    char input[500];
+    memset(input,'\0',sizeof(input));
+    cout<<"Welcome to use MiniSQL!"<<endl<<"-------------------------"<<endl<<endl;
+
+    statement SQL;
+    SQL.num=-1;
+
+    string database_name="master";       //记录选择数据库的名字
+
+    while(1)
+    {
+        cout<<"MinSQL->";
+        int t=0;
+        fflush(stdin);
+        while(cin.peek()!=';')
+        {
+            if((input[t++]=cin.get()) == '\n')
+                cout<<"MinSQL->";
+        }
+        fflush(stdin);
+
+        SQL=Read_Interpreter(input);   //将用户输入解析成内置语句
+
+        if(SQL.num==0)                   //如果输入错误则返回错误原因，并重置，等待下一个命令的输入
+        {
+            cout<<"error: SQL语句输入语法错误！"<<endl;
+            memset(input,'\0',sizeof(input));
+            SQL.num=-1;
+        }
+        else if(SQL.num==9)
+        {
+            break;
+        }
+        else
+        {
+            Excute_SQL(SQL,database_name);
+            memset(input,'\0',sizeof(input));
+            SQL.num=-1;
+        }
+    }
+
+    return 0;
+}
